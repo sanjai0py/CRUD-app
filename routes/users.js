65098795bb5aa60761af0302a0d88a5a1658c2b1
Sanjai Kumar S <sanjailucifer666@gmail.com>
2023-05-03 11:09:17 +0530

    added remaing features after CR in CRUD

--- routes/users.js
+++ routes/users.js
@@ -34,13 +34,40 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// Update user
-// router.put('/:id', async (req, res) => {
-//     try {
-        
-//     } catch (error) {
-        
-//     }
-// })
+// Updating user data with id
+router.put("/:id", async (req, res) => {
+  try {
+    let { id } = req.params;
+    var user = await Users.findByIdAndUpdate(id, req.body);
 
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "cannot find user with provided ID" });
+    }
+
+    const updatedUser = await Users.findById(id);
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500);
+  }
+});
+
+// Deleting user with ID
+router.delete("/:id", async (req, res) => {
+  try {
+    let { id } = req.params;
+    var user = await Users.findByIdAndDelete(id);
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "cannot find user with provided ID" });
+    }
+    
+    res.status(200).json({ message: "user with ID deleted" });
+  } catch (error) {
+    res.status(500);
+  }
+});
 export default router;
